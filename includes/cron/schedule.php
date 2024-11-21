be9b8a2511ad82a7f3b420dfe75ffb04640a6713
AgarthaTechnology <153688207+AgarthaTechnology@users.noreply.github.com>
2024-11-21 17:32:28 -0300

    Create schedule.php

---
+++ includes/cron/schedule.php
@@ -0,0 +1,10 @@
+<?php
+function kame_erp_schedule_inventory_sync() {
+    // Schedule inventory sync logic
+}
+
+function kame_erp_add_custom_intervals($schedules) {
+    // Add custom intervals logic
+}
+
+add_action('admin_init', 'kame_erp_schedule_inventory_sync');
