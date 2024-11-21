e0962883fd19320f5bab91a059f9bde10a53a91a
AgarthaTechnology <153688207+AgarthaTechnology@users.noreply.github.com>
2024-11-21 17:31:15 -0300

    Create sync.php

---
+++ includes/api/sync.php
@@ -0,0 +1,8 @@
+<?php
+function kame_erp_get_stock_by_warehouse($warehouse_name) {
+    // Get stock by warehouse logic
+}
+
+function kame_erp_synchronize_inventory() {
+    // Synchronize inventory logic
+}
