8c8a1024c3be967822a5b0fcbe82df14d508e72f
AgarthaTechnology <153688207+AgarthaTechnology@users.noreply.github.com>
2024-11-21 14:12:14 -0300

    Create functions.php

---
+++ includes/admin/functions.php
@@ -0,0 +1,16 @@
+<?php
+function kame_erp_functions_settings_init() {
+    // Register function settings here
+}
+
+function kame_erp_functions_section_callback() {
+    echo '<p>Puedes activar o desactivar las funciones integradas del plugin según tus necesidades.</p>';
+}
+
+function kame_erp_enable_integration_callback() {
+    // Callback for enabling integration
+}
+
+function kame_erp_enable_sync_callback() {
+    // Callback for enabling sync
+}
