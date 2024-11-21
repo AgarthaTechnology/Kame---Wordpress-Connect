11bc15c4ffee82bc3b74f8c6fb2aecb902797431
AgarthaTechnology <153688207+AgarthaTechnology@users.noreply.github.com>
2024-11-21 14:11:15 -0300

    Create menu.php

---
+++ includes/admin/menu.php
@@ -0,0 +1,24 @@
+<?php
+function kame_erp_menu() {
+    // Add admin menu items here
+}
+
+function kame_erp_config_page() {
+    // Display configuration page content here
+}
+
+function kame_erp_functions_page() {
+    // Display functions page content here
+}
+
+function kame_erp_inventory_page() {
+    // Display inventory page content here
+}
+
+function kame_erp_import_products_page() {
+    // Display import products page content here
+}
+
+function kame_erp_credits_page() {
+    // Display credits page content here
+}
