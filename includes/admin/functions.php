46b1f8e30829af1aec4cf48f6a3561962e1d3539
AgarthaTechnology <153688207+AgarthaTechnology@users.noreply.github.com>
2024-11-21 20:02:19 -0300

    Update functions.php

--- includes/admin/functions.php
+++ includes/admin/functions.php
@@ -1,6 +1,30 @@
 <?php
 function kame_erp_functions_settings_init() {
-    // Registrar configuraciones de funciones aquí
+    add_settings_section(
+        'kame_erp_functions_section',
+        'Configuración de Funciones de KAME ERP',
+        'kame_erp_functions_section_callback',
+        'kame_erp_functions'
+    );
+
+    add_settings_field(
+        'kame_erp_enable_integration',
+        'Habilitar Integración',
+        'kame_erp_enable_integration_callback',
+        'kame_erp_functions',
+        'kame_erp_functions_section'
+    );
+
+    add_settings_field(
+        'kame_erp_enable_sync',
+        'Habilitar Sincronización',
+        'kame_erp_enable_sync_callback',
+        'kame_erp_functions',
+        'kame_erp_functions_section'
+    );
+
+    register_setting('kame_erp_functions', 'kame_erp_enable_integration');
+    register_setting('kame_erp_functions', 'kame_erp_enable_sync');
 }
 
 function kame_erp_functions_section_callback() {
@@ -8,9 +32,13 @@ function kame_erp_functions_section_callback() {
 }
 
 function kame_erp_enable_integration_callback() {
-    // Callback para habilitar la integración
+    $checked = get_option('kame_erp_enable_integration') ? 'checked' : '';
+    echo '<input type="checkbox" name="kame_erp_enable_integration" ' . $checked . '>';
 }
 
 function kame_erp_enable_sync_callback() {
-    // Callback para habilitar la sincronización
+    $checked = get_option('kame_erp_enable_sync') ? 'checked' : '';
+    echo '<input type="checkbox" name="kame_erp_enable_sync" ' . $checked . '>';
 }
+
+add_action('admin_init', 'kame_erp_functions_settings_init');
