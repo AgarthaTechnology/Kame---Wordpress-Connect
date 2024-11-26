7b0f121103e3c5541312deeff96bb8f5e9d2b499
AgarthaTechnology <153688207+AgarthaTechnology@users.noreply.github.com>
2024-11-26 13:30:06 -0300

    Update error_log_sync_cron.php

--- error_log_sync_cron.php
+++ error_log_sync_cron.php
@@ -1,7 +1,1 @@
-[26-Nov-2024 14:30:01 UTC] PHP Warning:  Undefined array key "HTTP_HOST" in /home/admingalinea/staging.galinea.cl/wp-config.php on line 45
-[26-Nov-2024 14:35:01 UTC] PHP Warning:  Undefined array key "HTTP_HOST" in /home/admingalinea/staging.galinea.cl/wp-config.php on line 45
-[26-Nov-2024 14:40:01 UTC] PHP Warning:  Undefined array key "HTTP_HOST" in /home/admingalinea/staging.galinea.cl/wp-config.php on line 45
-[26-Nov-2024 14:45:01 UTC] PHP Warning:  Undefined array key "HTTP_HOST" in /home/admingalinea/staging.galinea.cl/wp-config.php on line 45
-[26-Nov-2024 14:50:01 UTC] PHP Warning:  Undefined array key "HTTP_HOST" in /home/admingalinea/staging.galinea.cl/wp-config.php on line 45
-[26-Nov-2024 14:55:01 UTC] PHP Warning:  Undefined array key "HTTP_HOST" in /home/admingalinea/staging.galinea.cl/wp-config.php on line 45
-[26-Nov-2024 15:00:01 UTC] PHP Warning:  Undefined array key "HTTP_HOST" in /home/admingalinea/staging.galinea.cl/wp-config.php on line 45
+
