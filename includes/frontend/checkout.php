89ebaca46637bdbc44fc4353ada7f1db17f07267
AgarthaTechnology <153688207+AgarthaTechnology@users.noreply.github.com>
2024-11-26 13:56:07 -0300

    Update checkout.php

--- includes/frontend/checkout.php
+++ includes/frontend/checkout.php
@@ -24,13 +24,25 @@ add_action('woocommerce_checkout_process', 'validar_campos_factura');
 add_action('woocommerce_checkout_update_order_meta', 'guardar_campos_factura');
 add_action('woocommerce_admin_order_data_after_billing_address', 'mostrar_campos_factura_admin', 10, 1);
 
+require_once __DIR__ . '/../api/connection.php';
+
 function enviar_pedido_a_kame_erp($order_id) {
     // Obtener el token de acceso
     $access_token = get_option('kame_erp_access_token');
+    $token_expiration = get_option('kame_erp_token_expiration', 0);
 
-    if (!$access_token) {
-        error_log('Token de acceso no disponible.');
-        return;
+    // Verificar si el token ha expirado
+    if (time() >= $token_expiration) {
+        $token_result = fetch_and_store_kame_erp_access_token();
+
+        if (!$token_result['success']) {
+            error_log("Error al obtener el token de acceso: " . $token_result['message']);
+            return;
+        }
+
+        // Obtener el nuevo token
+        $access_token = get_option('kame_erp_access_token');
+        $token_expiration = get_option('kame_erp_token_expiration', 0);
     }
 
     // Obtener los datos del pedido
