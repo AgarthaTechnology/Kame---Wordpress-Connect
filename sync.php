93b60515d52edd12cab4c7c3d6843a9cbb4187cf
AgarthaTechnology <153688207+AgarthaTechnology@users.noreply.github.com>
2024-11-26 13:26:32 -0300

    Delete sync.php

--- sync.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Incluye WordPress para acceder a sus funciones
-require_once('/path/to/your/wp-load.php'); // Ajusta esta ruta
-
-// Incluye el archivo de sincronización
-require_once plugin_dir_path(__FILE__) . 'includes/api/sync.php';
-
-// Ejecuta la sincronización
-kame_erp_synchronize_inventory();
